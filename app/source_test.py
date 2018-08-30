71ac432f4e216c5f42e8051526073656c166e7fc
wangui.njoka <wangui.njoka@gmail.com>
2018-08-30 18:24:09 +0300

    added unittest to test source

---
+++ app/source_test.py
@@ -0,0 +1,19 @@
+import uniitest
+from models import source
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source('cnn','CNN')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+
+if __name__ == '__main__':
+    unittest.main()

6bc4310958cc00f8948d22a4d4b9d8be6f7d9237
wangui.njoka <wangui.njoka@gmail.com>
2018-08-30 17:37:52 +0300

    added dynamic routes to source model

--- app/views.py
+++ app/views.py
@@ -9,13 +9,13 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-    message = 'Flash new'
-    return render_template('index.html',message = message)
+    title = 'HOME - LASTEST WORLD NEW'
+    return render_template('index.html', title = title)
 
-# @app.route('/articles/<_id>')
-# def source(source_id):
-#
-#     '''
-#     view article page function that returns the articles details page and its data
-#     '''
-#     return render_template('source.html',id = source)
+@app.route('/source/<int:source_id>')
+def source(source_id):
+
+    '''
+    view article page function that returns the source details page and its data
+    '''
+    return render_template('source.html',id = source_id)

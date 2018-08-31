010d634c5ae8e64cc8147ea321b177259f8a73b0
wangui.njoka <wangui.njoka@gmail.com>
2018-08-31 16:51:08 +0300

    added bootstrap and navbar linked it to base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__,instance_relative_config = True)
 
 #Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#Initializing flask extensions
+bootstrap = Bootstrap(app)
 
 
 from app import views
